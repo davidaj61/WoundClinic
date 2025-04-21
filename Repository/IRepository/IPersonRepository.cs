3e873af7d4d2724bbd90beab60b3afc4ee6fd7b9
davidaj61 <davood.a.j@gmail.com>
2025-04-21 22:41:07 +0330

    14040201-1445

---
+++ Repository/IRepository/IPersonRepository.cs
@@ -0,0 +1,19 @@
+﻿using WoundClinic.Data;
+
+namespace WoundClinic.Repository.IRepository
+{
+    public interface IPersonRepository
+    {
+        public Person Create(Person person);
+
+        public Person Update(Person person);
+
+        public Person Delete(Person person);
+
+        public Person Get(long id);
+        
+        public IEnumerable<Person> GetAll();
+
+
+    }
+}
---
+++ Repository/Person/PersonRepository.cs
@@ -0,0 +1,51 @@
+﻿using WoundClinic.Data;
+using WoundClinic.Repository.IRepository;
+
+namespace WoundClinic.Repository
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public PersonRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Person Create(Person person)
+        {
+            _db.Persons.Add(person);
+            _db.SaveChanges();
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            _db.Persons.Update(person);
+            _db.SaveChanges();
+            return person;
+        }
+        public bool Delete(Person person)
+        {
+            _db.Persons.Remove(person);
+            _db.SaveChanges();
+            return true;
+
+        }
+
+        public Person Get(long id)
+        {
+            var person = _db.Persons.FirstOrDefault(x => x.NationalCode == id);
+            if (person == null)
+            {
+                return new Person();
+            }
+            return person;    
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return _db.Persons.ToList();
+        }
+
+    }
+}
