d0370aa7023100d48dc97a001b3d76228f4d8bc9
davidaj61 <davood.a.j@gmail.com>
2025-05-10 06:25:54 +0330

    14040220-0135

---
+++ Services/Shared/SweetAlertServices.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+using static Microsoft.JSInterop.IJSRuntime;
+
+
+namespace WoundClinic.Services.Shared
+{
+    public class SweetAlertServices
+    {
+        IJSRuntime JSRuntime;
+        public SweetAlertServices(IJSRuntime _jSRuntime)
+        {
+            JSRuntime = _jSRuntime;
+        }
+        public async Task ShowModal(string title, string message,string icon)
+        {
+            JSRuntime.InvokeVoidAsync("Swal.fire", new
+            {
+                title = title,
+                text = message,
+                icon = icon
+            });
+        }
+
+    }
+}
